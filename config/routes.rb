54e11e29bc43c3b8367bff5a13c0c036a455ff6b
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-01-19 13:42:00 +0000

    clean sign_up/log_in

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :public do
+    resources :users, only: [:show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
