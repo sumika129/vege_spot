4efdfb2ee96f31001a96ee95de329f9221f28fa7
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-01-23 15:39:48 +0000

    update user/edit,add public/homes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
+  root to: "public/homes#top"
+
   namespace :public do
+    get 'home/about'=>'homes#about',as:'about'
     resources :users, only: [:show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
