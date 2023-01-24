76b578b74ec98dd4ee3b3808bacfa86c628862b5
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-01-24 14:27:28 +0000

    add admin/category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/edit'
+  end
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Admin.create!(
+   email: 'kznknd.dkhtt.146@gmail.com',
+   password: 'hyde78'
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
