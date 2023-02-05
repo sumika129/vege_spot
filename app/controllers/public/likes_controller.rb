9dfd6592731e5c8475a2cb9ce76e98c23cce4867
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-02-05 15:35:41 +0000

    update likes

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,11 +1,17 @@
 class Public::LikesController < ApplicationController
 
   def create
-
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.new(@post.id)
+    like.save
+    redirect_to public_post_path(@post)
   end
 
   def destroy
-
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.find_by(@post.id)
+    @like.destroy
+    redirect_to public_post_path(params[:post_id])
   end
 
 end
