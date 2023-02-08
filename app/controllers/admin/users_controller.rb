bfad8642279d173fb9c5e95d8c54a762eb173f96
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-02-08 13:04:05 +0000

    add admin/user

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories, only: [:index, :create, :edit, :update]
     resources :areas, only: [:index, :create, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
