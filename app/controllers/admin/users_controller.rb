76ceb3139a61f34a87bf71f73a685bce6f6ad555
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-02-09 11:09:30 +0000

    update admin user/edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @post = @user.posts
   end
 
   def edit
@@ -12,5 +13,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id)
+    else
+      render:edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
