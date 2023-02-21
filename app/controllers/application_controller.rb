526b68613eb7cb56b8a463649ef236dccfe1399a
sumika129 <112753628+sumika129@users.noreply.github.com>
2023-02-21 13:04:37 +0000

    update admin/session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  #before_action :authenticate_user!, except: [:top, :about,]
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -7,4 +9,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+        root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+        root_path
+    end
+  end
+
+
 end
